23977d7031a4cd333264f7624e31dc0093582bd3
Josip <joske0000@gmail.com>
2023-09-19 21:33:07 +0200

    RBA

--- RBA/src/main/java/com/example/api/CreditCardProcess.java
+++ RBA/src/main/java/com/example/api/CreditCardProcess.java
@@ -2,7 +2,6 @@ package com.example.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
