43380779627cb96b86d45b8a16cd442c11cb68fe
Josip <joske0000@gmail.com>
2023-09-23 22:06:29 +0200

    RBA

--- RBA/src/main/java/com/example/api/CreditCardProcess.java
+++ RBA/src/main/java/com/example/api/CreditCardProcess.java
@@ -6,53 +6,61 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 @RequestMapping("/creditCard")
+@ImportResource("classpath:config/liquibase/master.xml")
 public class CreditCardProcess {
 
-    @PostMapping("/addPerson")
-    public ActionResult addPersonForCreatingCreditCard(@RequestBody Person person) {
-        insertIntoDB(person);
-        return null;
+  @PostMapping("/addPerson")
+  public ActionResult addPersonForCreatingCreditCard(
+    @RequestBody Person person
+  ) {
+    insertIntoDB(person);
+    return null;
+  }
+
+  private ActionResult insertIntoDB(Person person) {
+    String sql =
+      "INSERT INTO PERSONS (NAME, SURNAME, OIB, STATUS) VALUES (?, ?, ?, ?)";
+    try {
+      String url = "${spring.datasource.url}";
+      String username = "${spring.datasource.username}";
+      String password = "${spring.datasource.password}";
+
+      Connection connection = DriverManager.getConnection(url, username, password);
+
+      PreparedStatement preparedStatement = connection.prepareStatement(sql);
+      preparedStatement.setString(1, person.getName());
+      preparedStatement.setString(2, person.getSurname());
+      preparedStatement.setString(3, person.getOib());
+      preparedStatement.setString(4, "ACTIVE");
+
+      int rowCount = preparedStatement.executeUpdate();
+
+      if (rowCount > 0) {
+        ActionResult result = new ActionResult();
+        result.setStatus(ActionResult.StatusCode.OK);
+        result.setMessage("Rows updated!");
+        return result;
+      }
+    } catch (final Exception e) {
+      ActionResult result = new ActionResult();
+      result.setStatus(ActionResult.StatusCode.ERROR);
+      result.setMessage("Rows not updated!");
+      return result;
     }
+    return null;
+  }
 
-    private ActionResult insertIntoDB(Person person) {
-        String sql = "INSERT INTO PERSONS (NAME, SURNAME, OIB, STATUS) VALUES (?, ?, ?, ?)";
-        try {
-
-            String url = "${spring.datasource.url}";
-            String username = "${spring.datasource.username}";
-            String password = "${spring.datasource.password}";
-
-            Connection veza = DriverManager.getConnection(url, username, password);
-
-            PreparedStatement preparedStatement = veza.prepareStatement(upit);
-            preparedStatement.setString(1, person.getName());
-            preparedStatement.setString(2, person.getSurname());
-            preparedStatement.setString(3, person.getOib());
-            preparedStatement.setString(4, "ACTIVE");
-
-            int brojRedova = preparedStatement.executeUpdate();
-
-            if (brojRedova > 0) {
-                ActionResult result = new ActionResult();
-                result.setStatus(ActionResult.StatusCode.OK);
-                result.setMessage("Rows updated!");
-                return result;
-            }
-        } catch (final Exception e) {
-            return null;
-
-        }
-
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(CreditCardProcess.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(CreditCardProcess.class, args);
+  }
 }
